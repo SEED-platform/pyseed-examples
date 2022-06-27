799aa4ba2adbca8b469e78c3a03d5ebe18f8182f
Nicholas Long <nicholas.long@nrel.gov>
2022-06-27 18:40:42 -0400

    precommit

--- examples/seed_better.py
+++ examples/seed_better.py
@@ -1,18 +1,18 @@
 # Example to read and sync seed properties with salesforce
 
 # ./manage.py create_test_user_json --username nicholas.long@nrel.gov --file ../py-seed/seed-config.json --pyseed
-# Make sure that the host is localhost and not 127.0.0.1. For some reason the organization endpoints only work on 
+# Make sure that the host is localhost and not 127.0.0.1. For some reason the organization endpoints only work on
 # localhost.
 
-from pathlib import Path
-from pyseed.seed_client import SeedClient
 from datetime import date
+from pathlib import Path
 
+from pyseed.seed_client import SeedClient
 
 seed_config = Path('seed-config-dev.json')
 data_dir = Path(__file__).parent.absolute() / 'data' / 'better'
 
-client = SeedClient(None,            connection_config_filepath=seed_config)    
+client = SeedClient(None, connection_config_filepath=seed_config)
 client.get_org_by_name('better', set_org_id=True)
 
 # get or create the cycle
@@ -20,9 +20,9 @@ cycle = client.get_or_create_cycle('BETTER Cycle', date(2020, 1, 1), date(2020, 
 
 # upload the data to seed
 client.upload_and_match_datafile(
-    'better-data', 
-    str(data_dir / 'better-test-data.xlsx'), 
-    'better mappings', 
+    'better-data',
+    str(data_dir / 'better-test-data.xlsx'),
+    'better mappings',
     str(data_dir / 'better-mappings.csv'),
     import_meters_if_exist=True,
 )
--- examples/seed_bps.py
+++ examples/seed_bps.py
@@ -2,15 +2,16 @@
 
 # # ./manage.py create_test_user_json --username nicholas.long@nrel.gov --file ../py-seed/seed-config.json --pyseed
 
-from pathlib import Path
 from datetime import date
+from pathlib import Path
+
 from pyseed.seed_client import SeedClient
 
 seed_config = Path('seed-config-dev.json')
 data_dir = Path(__file__).parent.absolute() / 'data' / 'bps'
 
 client = SeedClient(
-    None, 
+    None,
     connection_config_filepath=seed_config,
 )
 # You will need to make sure that the org name is already
@@ -24,16 +25,16 @@ cycle_2025 = client.get_or_create_cycle('2025', date(2024, 1, 1), date(2025, 12,
 # set the active cycle and upload to that cycle
 client.cycle_id = cycle_2023['id']
 client.upload_and_match_datafile(
-    'bps-data', 
-    str(data_dir / 'building-performance-standards-sample-2023.xlsx'), 
-    'bps mappings', 
+    'bps-data',
+    str(data_dir / 'building-performance-standards-sample-2023.xlsx'),
+    'bps mappings',
     str(data_dir / 'bps-mappings.csv')
 )
 
 client.cycle_id = cycle_2024['id']
 client.upload_and_match_datafile(
-    'bps-data', 
-    str(data_dir / 'building-performance-standards-sample-2024.xlsx'), 
-    'bps mappings', 
+    'bps-data',
+    str(data_dir / 'building-performance-standards-sample-2024.xlsx'),
+    'bps mappings',
     str(data_dir / 'bps-mappings.csv')
 )
